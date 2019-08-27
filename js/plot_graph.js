77617cb3839618fb4576ccfa7e5f293fdddabf24
Arkorn Poonpon <gatty2012@gmail.com>
2019-08-27 14:27:17 +0700

    addd graph work plan

--- js/plot_graph.js
+++ js/plot_graph.js
@@ -1,37 +1,141 @@
 $('document').ready(function () {
-    $("#chart-container").insertFusionCharts({
-        type: "column2d",
-        width: "100%",
-        height: "400",
-        dataFormat: "json",
-        dataSource: {
-            // Chart Configuration
-            "chart": {
-                "caption": "จำนวนยอดไลก์เฟซบุ๊กเพจเกี่ยวกับการวิ่ง",
-                "subCaption": "ณ วันที่ 26 สิงหาคม 2562",
-                "xAxisName": "ชื่อเพจ",
-                "yAxisName": "จำนวนยอดคนกดไลก์",
-                "numberSuffix": "K",
-                "theme": "fusion",
-                "baseFont": "Kanit",
 
+  $("#chart-container").insertFusionCharts({
+    type: "scrollcolumn2d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "ยอดไลก์และติดตามของ 10 อันดับเพจเกี่ยวกับการวิ่ง",
+        subcaption: "ณ วันที่ 27 สิงหาคม 2562",
+        yaxisname: "ยอดการกดไลก์และติดตาม",
+        numvisibleplot: "22",
+        formatNumber: "0",
+        labeldisplay: "auto",
+        theme: "fusion",
+        baseFont: "Kanit"
+      },
+      categories: [
+        {
+          category: [
+            {
+              label: "วิ่งไหนดี"
             },
-            // Chart Data
-            "data": [{
-                "label": "Thairun",
-                "value": "642.482"
-            }, {
-                "label": "วิ่งไหนดี",
-                "value": "770.927"
-            }, {
-                "label": "ออกไปวิ่ง",
-                "value": "95.000"
-            }, {
-                "label": "นักวิ่งน่ารัก",
-                "value": "111.708"
-            }]
+            {
+              label: "ThaiRun"
+            },
+            {
+              label: "ไปวิ่งกันเถอะ"
+            },
+            {
+              label: "เรื่องวิ่งเรื่องกล้วย"
+            },
+            {
+              label: "Runlah"
+            },
+            {
+              label: "วิ่งไหนกัน ปั่นไหนดี"
+            },
+            {
+              label: "นักวิ่งน่ารัก"
+            },
+            {
+              label: "ออกไปวิ่ง"
+            },
+            {
+              label: "ไปวิ่งกัน"
+            },
+            {
+              label: "ไปวิ่ง"
+            },
+            {
+              label: "Running thailand events"
+            }
+          ]
         }
-    });
+      ],
+      dataset: [
+        {
+          seriesname: "Liked",
+          data: [
+            {
+              value: "777818"
+            },
+            {
+              value: "642590"
+            },
+            {
+              value: "132346"
+            },
+            {
+              value: "130302"
+            },
+            { 
+              value: "116865"
+            },
+            {
+              value: "114320"
+            },
+            {
+              value: "111707"
+            },
+            {
+              value: "95006"
+            },
+            {
+              value: "94171"
+            },
+            {
+              value: "73942"
+            },
+            {
+              value: "34589"
+            }
+          ]
+        },
+        {
+          seriesname: "followed",
+          data: [
+            {
+              value: "782537"
+            },
+            {
+              value: "660562"
+            },
+            {
+              value: "136148"
+            },
+            {
+              value: "131228"
+            },
+            {
+              value: "119513"
+            },
+            {
+              value: "117264"
+            },
+            {
+              value: "114395"
+            },
+            {
+              value: "95006"
+            },
+            {
+              value: "104886"
+            },
+            {
+              value: "74940"
+            },
+            {
+              value: "36279"
+            }
+          ]
+        }
+      ]
+    }
+  });
+  
 
 
     $("#chart-container-stack").insertFusionCharts({
@@ -42,47 +146,235 @@ $('document').ready(function () {
         dataSource: {
             chart: {
             caption: "จำนวนยอดไลก์เพจที่เพิ่มขึ้นในระยะเวลา 4 วัน",
-            subCaption: "ตั้งแต่วันที่ 26 - 29 สิงหาคม 2562",
+            subCaption: "ตั้งแต่วันที่ 26 - 30 สิงหาคม 2562",
             yaxisname: "ยอดไลก์",
             flatscrollbars: "0",
             scrollheight: "12",
-            numvisibleplot: "8",
-            numberSuffix: "K",
+            numvisibleplot: "11",
+            formatNumber: "0",
             baseFont: "Kanit",
             plottooltext:
-                "<b>$dataValue</b> On $label",
+                "ยอดกดไลก์ : <b>$dataValue</b> ของเพจ  $label",
             theme: "fusion"
             },
             categories: [
             {
                 category: [
-                {
-                    label: "Thairun"
-                }
+                  {
+                    label: "วิ่งไหนดี"
+                  },
+                  {
+                    label: "ThaiRun"
+                  },
+                  {
+                    label: "ไปวิ่งกันเถอะ"
+                  },
+                  {
+                    label: "เรื่องวิ่งเรื่องกล้วย"
+                  },
+                  {
+                    label: "Runlah"
+                  },
+                  {
+                    label: "วิ่งไหนกัน ปั่นไหนดี"
+                  },
+                  {
+                    label: "นักวิ่งน่ารัก"
+                  },
+                  {
+                    label: "ออกไปวิ่ง"
+                  },
+                  {
+                    label: "ไปวิ่งกัน"
+                  },
+                  {
+                    label: "ไปวิ่ง"
+                  },
+                  {
+                    label: "Running thailand events"
+                  }
                 ]
             }
             ],
             dataset: [
             {
-                seriesname: "ยอดคนกดไลก์วันที่ 26",
+                seriesname: "ยอดคนกดไลก์วันที่ ณ วันที่ 26 สิงหาคม  2019",
                 data: [
-                {
-                    value: "642.48"
-                }
+                  {
+                    value: "777818"
+                  },
+                  {
+                    value: "642590"
+                  },
+                  {
+                    value: "132346"
+                  },
+                  {
+                    value: "130302"
+                  },
+                  { 
+                    value: "116865"
+                  },
+                  {
+                    value: "114320"
+                  },
+                  {
+                    value: "111707"
+                  },
+                  {
+                    value: "95006"
+                  },
+                  {
+                    value: "94171"
+                  },
+                  {
+                    value: "73942"
+                  },
+                  {
+                    value: "34589"
+                  }
                 ]
             },
             {
-                seriesname: "ยอดคนกดไลก์เพิ่มใน 4 วัน",
+                seriesname: "ยอดคนกดไลก์เพิ่มใน 5 วัน",
                 data: [
-                {
-                    value: "10"
-                }
+                  {
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  },
+                  { 
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  },
+                  {
+                    value: "1000"
+                  }
                 ]
             }
             ]
         }
         });
 
+
+        $("#chart-work-plan").insertFusionCharts({
+            type: "gantt",
+            width: "100%",
+            height: "500",
+            dataFormat: "json",
+            dataSource: {
+              chart: {
+                dateformat: "dd/mm/yyyy",
+                caption: "แผนงานที่กำหนด",
+                theme: "fusion",
+                canvasborderalpha: "40",
+                baseFont: "Kanit",
+                ganttlinealpha: "50"
+              },
+              tasks: {
+                color: "#5D62B5",
+                task: [
+                  {
+                    start: "26/08/2019",
+                    end: "27/08/2019"
+                  },
+                  {
+                    start: "26/08/2019",
+                    end: "28/08/2019"
+                  },
+                  {
+                    start: "28/08/2019",
+                    end: "29/08/2019"
+                  },
+                  {
+                    start: "29/08/2019",
+                    end: "30/08/2019"
+                  },
+                  {
+                    start: "30/08/2019",
+                    end: "31/08/2019"
+                  }
+                ]
+              },
+              processes: {
+                headertext: "Task",
+                headeralign: "left",
+                fontsize: "14",
+                isbold: "0",
+                align: "left",
+                process: [
+                  {
+                    label: "วางแผนงานทำงาน"
+                  },
+                  {
+                    label: "เริ่มสำรวจและเก็บข้อมูลพื้นฐาน<br>เพจเฟซบุ๊กที่เกี่ยวข้องกับการวิ่ง"
+                  },
+                  {
+                    label: "เริ่มเก็บข้อมูลเชิงลึกและกำหนดตัววัดคะแนน<br>ในแต่ละหัวข้อที่สนใจ"
+                  },
+                  {
+                    label: "สรุปข้อมูลจากการสำรวจทั้งหมด<br>ในรูปแบบข้อมูลและกราฟแสดงผล"
+                  },
+                  {
+                    label: "นำเสนอผลการดำเนินการทั้งหมด"
+                  }
+                ]
+              },
+              categories: [
+                {
+                  category: [
+                    {
+                      start: "26/08/2019",
+                      end: "27/08/2019",
+                      label: "Day 1"
+                    },
+                    {
+                        start: "27/08/2019",
+                        end: "28/08/2019",                      
+                        label: "Day 2"
+                    },
+                    {
+                        start: "28/08/2019",
+                        end: "29/08/2019",                      
+                        label: "Day 3"
+                    },
+                    {
+                        start: "29/08/2019",
+                        end: "30/08/2019",
+                        label: "Day 4"
+                    },
+                    {
+                        start: "30/08/2019",
+                        end: "31/08/2019",
+                        label: "Day 5"
+                    }
+                  ]
+                }
+              ]
+            }
+          });
+          
 
 
 
